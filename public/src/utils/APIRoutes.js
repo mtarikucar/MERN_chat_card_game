8249a48d86ea825b94e71f39e1684cfe742e9180
tarik.fi <tarik42777@gmail.com>
2023-02-23 14:15:47 +0300

    changed host

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:5000";
+export const host = "http://3.67.175.7:5000";
 export const loginRoute = `${host}/api/auth/login`;
 export const logoutRoute = `${host}/api/auth/logout`;
 export const allUsersRoute = `${host}/api/auth/allusers`;
